603d132c2bab738b612ce14394ced5f2809eb126
June <june@peanut.one>
2024-02-11 02:30:31 -0500

    Update naming in mane.

--- vulpes/blueprints/mane/views.py
+++ vulpes/blueprints/mane/views.py
@@ -1,6 +1,5 @@
 from datetime import datetime, timezone
 from threading import Thread
-from typing import Optional
 
 from flask import Blueprint, redirect, render_template, request, url_for
 from sqlalchemy import select
@@ -16,7 +15,7 @@ bp = Blueprint("mane", __name__, template_folder="templates")
 
 
 @bp.route("/")
-def mainpage():
+def main_page():
     """Get the site's homepage."""
     return render_template("mainpage.html")
 
@@ -30,57 +29,57 @@ def dropbox():
 @bp.route("/upload", methods=["POST"])
 def upload():
     """Handle file upload."""
-    f = request.files["file"]
+    file = request.files["file"]
     if request.form.get("dropbox"):
         Thread(
             target=send_file,
             # Can't use the decorator because it tries to get the jmap global
             # or the config to create it, from the app context. Doesn't exist
             # in a thread. We have to pull it out here and pass it along.
-            args=(get_jmap(), f.filename, f.read()),
+            args=(get_jmap(), file.filename, file.read()),
             daemon=True,
         ).start()
         return redirect(url_for("mane.dropbox"))
 
-    filename = performupload(f)
+    filename = perform_upload(file)
     if filename is not None:
         return render_template("fileuploaded.html", link=filename)
     return "Error, probably an empty upload field"
 
 
 @bp.route("/uploadbot", methods=["POST"])
-def uploadbot():
+def bot_upload():
     """Handle file upload and return a simpler response for bots."""
-    f = request.files["file"]
-    filename = performupload(f)
+    file = request.files["file"]
+    filename = perform_upload(file)
     if filename is not None:
         return "http://f.peanut.one/" + filename
     return "Error, probably an empty upload field"
 
 
-def performupload(f: FileStorage, customname: Optional[str] = None):
+def perform_upload(file: FileStorage, custom_name: str | None = None):
     """Rename and upload file to Amazon S3, returning its new name."""
-    if f is None or f.filename is None:
+    if file is None or file.filename is None:
         return None
 
-    filename = secure_filename(f.filename)
+    filename = secure_filename(file.filename)
     ext = [x[-1] if len(x) > 1 else None for x in [filename.split(".")]][0]
-    if customname is not None:
+    if custom_name is not None:
         result = db.session.execute(
             select(PeanutFile)
-            .where(PeanutFile.filename == customname),
+            .where(PeanutFile.filename == custom_name),
         ).fetchone()
         if result is not None:
             return None
-        newname = customname
+        newname = custom_name
         if ext:
             newname += "." + ext
     else:
         newname = randomname(ext)
 
-    f.seek(0, 2)
-    size = f.tell()
-    f.seek(0, 0)
+    file.seek(0, 2)
+    size = file.tell()
+    file.seek(0, 0)
     db.session.add(
         PeanutFile(
             filename=newname,
@@ -92,8 +91,7 @@ def performupload(f: FileStorage, customname: Optional[str] = None):
     db.session.commit()
     # amazon.upload(newname, f.stream)
     get_amazon().upload_fileobj(
-        f, "f.peanut.one", newname,
-        ExtraArgs={"ACL": "public-read", "ContentType": f.mimetype})
+        file, "f.peanut.one", newname,
+        ExtraArgs={"ACL": "public-read", "ContentType": file.mimetype})
 
-    # deletewithinquota(c)
     return newname
